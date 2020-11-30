0b03b1cbdc9245d5c75e889cdaddb32f5a465051
Reilly Becker <rbecker2@uncc.edu>
2020-11-29 22:08:33 -0500

    Update covid_cases_and_temp_data_join_on_fips.sql

--- query_examples/covid_cases_and_temp_data_join_on_fips.sql
+++ query_examples/covid_cases_and_temp_data_join_on_fips.sql
@@ -1,1 +1,1 @@
-SELECT city, state, covid_cases.date, total_population, new_cases, new_deaths FROM covid_cases INNER JOIN temp_data ON temp_data.fips_code = covid_cases.fips_code;
+SELECT avg_temperature_air_2m_f, city, state, covid_cases.date, total_population, new_cases, new_deaths FROM covid_cases INNER JOIN temp_data ON temp_data.fips_code = covid_cases.fips_code;
